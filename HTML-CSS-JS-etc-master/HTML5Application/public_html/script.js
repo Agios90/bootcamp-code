f13212f60ee629f2c81fa96eb70b679a1df5969d
Angelos <33423921+Agios90@users.noreply.github.com>
2018-02-15 13:16:43 +0200

    Add files via upload

---
+++ HTML-CSS-JS-etc-master/HTML5Application/public_html/script.js
@@ -0,0 +1,35 @@
+function koala() {
+			alert('KOALA! KOALA!');
+		};
+		
+function func1() {
+		
+		var catname = "Tom";
+		var mousename = "Jerry";
+		alert(catname+" and "+mousename);
+		
+};
+
+function add(ten, twenty) {
+		
+		alert(ten+twenty);
+		
+};
+
+var emperor = {
+	name: "Palpatine",
+	age: "114",
+	talk: function() { 
+		alert("Join me and together we shall rule the galaxy");
+	}
+};
+
+var handleClick = function (event) {
+	
+	var i = '1';
+	while (i < 4) {
+		alert(i);
+		i++;
+	}
+};
+
